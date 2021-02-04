277a0494bc7e4c0bbd85403b64c32a181ecec13c
yudhapn <yudhapnursam@gmail.com>
2021-02-04 17:01:49 +0700

    mod response field getitems & getitem resolver

--- client/src/util/graphql.js
+++ client/src/util/graphql.js
@@ -25,27 +25,26 @@ export const FETCH_POSTS_QUERY = gql`
 export const FETCH_ITEMS_QUERY = gql`
   {
     getItems{
-    id
-    name
-    price
-    createdAt
-    username
-    description
-    user {
-      seller {
-        username
-      }
-    }
-    images{
       id
-      src
-    }
-    reviewCount
-    bookmarkedBy{
-      id
-      userId
+      name
+      price
       createdAt
-    }
+      description
+      
+      images{
+        id
+        downloadUrl
+      }
+      bookmarkedBy{
+        id
+        userId
+        createdAt
+      }
+      user{
+        seller{
+          username
+        }
+      }
   }
 }
 `;
