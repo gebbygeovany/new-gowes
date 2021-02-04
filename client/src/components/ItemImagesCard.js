8392a38401ef686191ba3518d94342144e496cce
yudhapn <yudhapnursam@gmail.com>
2021-02-04 22:46:40 +0700

    refine show list images and selected image feature

--- client/src/components/ItemImagesCard.js
+++ client/src/components/ItemImagesCard.js
@@ -1,34 +1,21 @@
-import React from 'react';
-import { Card, Image, Sticky } from 'semantic-ui-react';
+import React, { useState } from 'react';
+import { Image, Sticky } from 'semantic-ui-react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/swiper.scss';
 
-function ItemImagesCard({ contextRef }) {
-    let objs = [
-        {id: 1, name:"john"},
-        {id: 2, name:"john"},
-        {id: 3, name:"john"},
-        {id: 4, name:"john"},
-        {id: 4, name:"john"},
-        {id: 4, name:"john"},
-        {id: 4, name:"john"},
-    ]
-    for (let index = 0; index < 8; index++) {
-        objs = [...objs, objs]
-    }
+function ItemImagesCard({ contextRef, images }) {
+    const [selectedImage, setSelectedImage] = useState(images[0].downloadUrl)
     return (
         <Sticky context={contextRef} offset={130}>
-        <Image src='https://react.semantic-ui.com/images/wireframe/image.png' style={{ marginBottom: 4 }}/>
+        <Image src={selectedImage} style={{ marginBottom: 6, width: 250, height: 250 }}/>
         <Swiper
-          spaceBetween={8}
+          spaceBetween={6}
           slidesPerView={4}
           onSlideChange={() => console.log('slide change')}
           onSwiper={(swiper) => console.log(swiper)}>
-            {objs.map((obj, index) => (
+            {images.map((image, index) => (
               <SwiperSlide key={index} virtualIndex={index}>
-              <Card>
-                <Image src='https://react.semantic-ui.com/images/wireframe/image.png' size='small' />
-              </Card>
+                <Image onClick={() => {setSelectedImage(image.downloadUrl)}} src={ image.downloadUrl } style={{ width: 60, height: 60 }}/>
             </SwiperSlide>
             )) }
         </Swiper>
