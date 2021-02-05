4e3e4053f9ff8526e430939074bfe51a139fab25
yudhapn <yudhapnursam@gmail.com>
2021-02-06 00:54:42 +0700

    add condition on chat floating button visible

--- client/src/components/ChatFloatingButton.js
+++ client/src/components/ChatFloatingButton.js
@@ -1,9 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { Icon, Transition, Segment, Card, Container } from 'semantic-ui-react';
+import { AuthContext } from '../context/auth';
 
 function ChatFloatingButton() {
     const [ visible, setVisible ] = useState(true)
     const toggleVisibility = () => {setVisible(!visible)}
+    const { user } = useContext(AuthContext);
+    
 
     const style = {
         borderRadius: 26,
@@ -32,26 +35,29 @@ function ChatFloatingButton() {
     };
 
     const spanStyle = { fontSize: 14, fontWeight: 'bold'};
-    return (
-        <Container>
-            <Transition visible={visible} animation='scale' duration={500}>
-                <Segment onClick={toggleVisibility} href="#" floated="right" raised="true" style={style}>
-                    <Icon color="teal" size="large" name="discussions"/>
-                    <span style={spanStyle}>Chat</span>
-                </Segment>
-            </Transition>
-            <Transition visible={!visible} animation='scale' duration={500}>
-            <Card onClick={toggleVisibility} href="#" style={cardStyle}>
-                <Card.Content header='About Amy' />
-                <Card.Content description="just description" />
-                <Card.Content extra>
-                <Icon name='user' />4 Friends
-                </Card.Content>
-            </Card>
-            </Transition>
-        </Container>
 
-
-    )
+    let chatFloatingMarkup  = (<br/>)
+    if (user !== null) {
+        chatFloatingMarkup = ( 
+            <Container>
+                <Transition visible={visible} animation='scale' duration={500}>
+                    <Segment onClick={toggleVisibility} href="#" floated="right" raised="true" style={style}>
+                        <Icon color="teal" size="large" name="discussions"/>
+                        <span style={spanStyle}>Chat</span>
+                    </Segment>
+                </Transition>
+                <Transition visible={!visible} animation='scale' duration={500}>
+                <Card onClick={toggleVisibility} href="#" style={cardStyle}>
+                    <Card.Content header='About Amy' />
+                    <Card.Content description="just description" />
+                    <Card.Content extra>
+                    <Icon name='user' />4 Friends
+                    </Card.Content>
+                </Card>
+                </Transition>
+            </Container>
+            )
+    }
+    return chatFloatingMarkup;
 }
 export default ChatFloatingButton
