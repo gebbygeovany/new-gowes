eb69892b8b503c5fce53cb63fb04609a3fd12e64
yudhapn <yudhapnursam@gmail.com>
2021-02-08 22:01:44 +0700

    mod chat floating card

--- client/src/components/ChatFloatingCard.js
+++ client/src/components/ChatFloatingCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Icon, Grid, Segment, List, Item, Image } from 'semantic-ui-react';
+import { Icon, Grid, Segment, List, Item, Image, Card, Feed } from 'semantic-ui-react';
 
 function ChatFloatingCard() {
     const topLeftBar = {
@@ -21,7 +21,7 @@ function ChatFloatingCard() {
         padding: 0,
         margin: 0,
         overflow: 'auto',
-        maxHeight: 312,
+        maxHeight: 342,
         borderTopLeftRadius: 0,
         borderTopRightRadius: 0,
         borderBottomLeftRadius: 12,
@@ -32,60 +32,128 @@ function ChatFloatingCard() {
         <Grid >
             <Grid.Column width={5} style={{ padding: 0}}>
                 <Segment style={topLeftBar}>
-                    <Item.Group>
-                        <Item>
-                            <Item.Image circular size='mini' src='https://react.semantic-ui.com/images/wireframe/image.png' />
-                            <Item.Content>
-                                <Item.Header>Header</Item.Header>
-                                <Item.Meta>Description</Item.Meta>
-                            </Item.Content>
-                        </Item> 
-                    </Item.Group>
+                    <List>
+                        <List.Item>
+                            <List.Content style={{ fontWeight: 'bold'}}>Chat</List.Content>
+                        </List.Item>
+                    </List>
                 </Segment>
                 <Segment style={leftContent}>
-                <Item.Group style={{ marginLeft: 16, paddingTop: 12 }}>
-                        <Item>
-                            <Image circular size='mini' src='https://react.semantic-ui.com/images/wireframe/image.png' />
-                            <Item.Content>
-                                <Item.Header>Header</Item.Header>
-                                <Item.Meta>Description</Item.Meta>
-                            </Item.Content>
+                    <Item.Group>
+                        <Item style={{ margin: 0 }}>
+                            <Card onClick={() => {}} style={{ borderRadius: 0, margin: 0, paddingTop: 8, paddingBottom: 8, paddingLeft: 4, paddingRight: 4 }}>
+                                <Feed>
+                                    <Feed.Event>
+                                        <Feed.Label>
+                                            <img src='https://react.semantic-ui.com/images/avatar/small/elliot.jpg' />
+                                        </Feed.Label>
+                                        <Feed.Content>
+                                            <Feed.Summary>
+                                                <Feed.User>Elliot Fu</Feed.User>
+                                            </Feed.Summary>
+                                        </Feed.Content>
+                                    </Feed.Event>
+                                </Feed>
+                            </Card>
                         </Item> 
-                        <Item>
-                            <Item.Image size='mini' src='https://react.semantic-ui.com/images/wireframe/image.png' />
-                            <Item.Content>
-                                <Item.Header>Header</Item.Header>
-                                <Item.Meta>Description</Item.Meta>
-                            </Item.Content>
+                       
+                        <Item style={{ margin: 0 }}>
+                            <Card onClick={() => {}} style={{ borderRadius: 0, margin: 0, paddingTop: 8, paddingBottom: 8, paddingLeft: 4, paddingRight: 4 }}>
+                                <Feed>
+                                    <Feed.Event>
+                                        <Feed.Label>
+                                            <img src='https://react.semantic-ui.com/images/avatar/small/elliot.jpg' />
+                                        </Feed.Label>
+                                        <Feed.Content>
+                                            <Feed.Summary>
+                                                <Feed.User>Elliot Fu</Feed.User>
+                                            </Feed.Summary>
+                                        </Feed.Content>
+                                    </Feed.Event>
+                                </Feed>
+                            </Card>
                         </Item> 
-                        <Item>
-                            <Item.Image size='mini' src='https://react.semantic-ui.com/images/wireframe/image.png' />
-                            <Item.Content>
-                                <Item.Header>Header</Item.Header>
-                                <Item.Meta>Description</Item.Meta>
-                            </Item.Content>
+                        <Item style={{ margin: 0 }}>
+                            <Card onClick={() => {}} style={{ borderRadius: 0, margin: 0, paddingTop: 8, paddingBottom: 8, paddingLeft: 4, paddingRight: 4 }}>
+                                <Feed>
+                                    <Feed.Event>
+                                        <Feed.Label>
+                                            <img src='https://react.semantic-ui.com/images/avatar/small/elliot.jpg' />
+                                        </Feed.Label>
+                                        <Feed.Content>
+                                            <Feed.Summary>
+                                                <Feed.User>Elliot Fu</Feed.User>
+                                            </Feed.Summary>
+                                        </Feed.Content>
+                                    </Feed.Event>
+                                </Feed>
+                            </Card>
                         </Item> 
-                        <Item>
-                            <Item.Image size='mini' src='https://react.semantic-ui.com/images/wireframe/image.png' />
-                            <Item.Content>
-                                <Item.Header>Header</Item.Header>
-                                <Item.Meta>Description</Item.Meta>
-                            </Item.Content>
+                        <Item style={{ margin: 0 }}>
+                            <Card onClick={() => {}} style={{ borderRadius: 0, margin: 0, paddingTop: 8, paddingBottom: 8, paddingLeft: 4, paddingRight: 4 }}>
+                                <Feed>
+                                    <Feed.Event>
+                                        <Feed.Label>
+                                            <img src='https://react.semantic-ui.com/images/avatar/small/elliot.jpg' />
+                                        </Feed.Label>
+                                        <Feed.Content>
+                                            <Feed.Summary>
+                                                <Feed.User>Elliot Fu</Feed.User>
+                                            </Feed.Summary>
+                                        </Feed.Content>
+                                    </Feed.Event>
+                                </Feed>
+                            </Card>
                         </Item> 
-                        <Item>
-                            <Item.Image size='mini' src='https://react.semantic-ui.com/images/wireframe/image.png' />
-                            <Item.Content>
-                                <Item.Header>Header</Item.Header>
-                                <Item.Meta>Description</Item.Meta>
-                            </Item.Content>
+                        <Item style={{ margin: 0 }}>
+                            <Card onClick={() => {}} style={{ borderRadius: 0, margin: 0, paddingTop: 8, paddingBottom: 8, paddingLeft: 4, paddingRight: 4 }}>
+                                <Feed>
+                                    <Feed.Event>
+                                        <Feed.Label>
+                                            <img src='https://react.semantic-ui.com/images/avatar/small/elliot.jpg' />
+                                        </Feed.Label>
+                                        <Feed.Content>
+                                            <Feed.Summary>
+                                                <Feed.User>Elliot Fu</Feed.User>
+                                            </Feed.Summary>
+                                        </Feed.Content>
+                                    </Feed.Event>
+                                </Feed>
+                            </Card>
                         </Item> 
-                        <Item>
-                            <Item.Image size='mini' src='https://react.semantic-ui.com/images/wireframe/image.png' />
-                            <Item.Content>
-                                <Item.Header>Header</Item.Header>
-                                <Item.Meta>Description</Item.Meta>
-                            </Item.Content>
+                        <Item style={{ margin: 0 }}>
+                            <Card onClick={() => {}} style={{ borderRadius: 0, margin: 0, paddingTop: 8, paddingBottom: 8, paddingLeft: 4, paddingRight: 4 }}>
+                                <Feed>
+                                    <Feed.Event>
+                                        <Feed.Label>
+                                            <img src='https://react.semantic-ui.com/images/avatar/small/elliot.jpg' />
+                                        </Feed.Label>
+                                        <Feed.Content>
+                                            <Feed.Summary>
+                                                <Feed.User>Elliot Fu</Feed.User>
+                                            </Feed.Summary>
+                                        </Feed.Content>
+                                    </Feed.Event>
+                                </Feed>
+                            </Card>
                         </Item> 
+                        <Item style={{ margin: 0 }}>
+                            <Card onClick={() => {}} style={{ borderRadius: 0, margin: 0, paddingTop: 8, paddingBottom: 8, paddingLeft: 4, paddingRight: 4 }}>
+                                <Feed>
+                                    <Feed.Event>
+                                        <Feed.Label>
+                                            <img src='https://react.semantic-ui.com/images/avatar/small/elliot.jpg' />
+                                        </Feed.Label>
+                                        <Feed.Content>
+                                            <Feed.Summary>
+                                                <Feed.User>Elliot Fu</Feed.User>
+                                            </Feed.Summary>
+                                        </Feed.Content>
+                                    </Feed.Event>
+                                </Feed>
+                            </Card>
+                        </Item> 
+                       
                     </Item.Group>
                 </Segment>
             </Grid.Column>
