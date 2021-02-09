59388d9845ffe07ad7625e26203af338be3840b1
Muhammad Gebby Geovany <mg.geovany@student.ub.ac.id>
2021-02-09 22:10:15 +0700

    tag ref added

--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -33,33 +33,32 @@ function Cart() {
   if (!loading) {
     if (cartItems.length !== 0) {
       let group = cartItems.reduce((r, a) => {
-        // console.log("a", a);
-        // console.log('r', r);
         r[a.item.user.id] = [...r[a.item.user.id] || [], a];
         return r;
       }, {});
       console.log(group)
-      // group.flatMap((item)=>console.log(item))
 
-      Object.keys(group).map(function(key, index){
+      Object.keys(group).map(function (key, index) {
         console.log(group[key])
       })
       cartMarkup = (
-        <Grid stackable>
-          <Grid.Column width={16}></Grid.Column>
-          <Grid.Column width={12} >
-            {group &&
-              Object.keys(group).map((key, index) => 
-              (
-                <CartCard cartItem={group[key]}></CartCard>
-              )
-              )
-            }
-          </Grid.Column>
-          <Grid.Column width={4}>
-            <ItemSummaryCard contextRef={contextRef}  ></ItemSummaryCard>
-          </Grid.Column>
-        </Grid>
+        <Ref innerRef={contextRef}>
+          <Grid stackable>
+            <Grid.Column width={16}></Grid.Column>
+            <Grid.Column width={12} >
+              {group &&
+                Object.keys(group).map((key, index) =>
+                (
+                  <CartCard cartItem={group[key]}></CartCard>
+                )
+                )
+              }
+            </Grid.Column>
+            <Grid.Column width={4}>
+              <ItemSummaryCard contextRef={contextRef}  ></ItemSummaryCard>
+            </Grid.Column>
+          </Grid>
+        </Ref>
       )
     }
 
